ffd1a56ebf4e2da837b3326deb2ba561c98c3a70
skotperez <voraginebramante@gmail.com>
2016-10-13 08:13:51 +0200

    Don't show footer in home page

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
 
 	</div><!-- #content -->
 
+<?php if ( !is_home() && !is_front_page() ) { ?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
@@ -20,6 +21,7 @@
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
+<?php } ?>
 
 <?php wp_footer(); ?>
 
